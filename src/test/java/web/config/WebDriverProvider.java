a8f3b9e63fd28faaf7ffd93efec306ff910d700f
AlinaPapsueva <98316705+AlinaPapsueva@users.noreply.github.com>
2023-03-26 21:05:43 +0300

    fix video

--- src/test/java/web/config/WebDriverProvider.java
+++ src/test/java/web/config/WebDriverProvider.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Configuration;
 import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.Map;
+
 public class WebDriverProvider {
 
     private WebDriverConfig config;
@@ -14,28 +16,19 @@ public class WebDriverProvider {
     }
 
     private void createWebDriver() {
-        switch (config.getBrowser().toLowerCase()) {
-            case "chrome":
-                Configuration.browser = "chrome";
-                break;
-            case "edge":
-                Configuration.browser = "edge";
-                break;
-            case "opera":
-                Configuration.browser = "opera";
-                break;
-            default:
-                throw new RuntimeException(config.getBrowser());
-        }
 
         Configuration.baseUrl = config.getBaseUrl();
+        Configuration.browser = config.getBrowser();
         Configuration.browserVersion = config.getBrowserVersion();
         Configuration.browserSize = config.getBrowserSize();
         Configuration.remote = config.getRemoteUrl();
         Configuration.pageLoadStrategy = config.getPageLoadStrategy();
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 }
